9c438211af6c66d5001719f4e99430b8e31a8cd8
dwcmcdonough <daniel.mcdonough@gordon.edu>
2023-11-30 20:44:06 -0500

    ReadME changes

--- extracting_date_regex.R
+++
@@ -1,6 +0,0 @@
-# playing around with dates of birth and death
-
-lotr_data_test2 |>
-  mutate(birth_date = str_extract(birth, pattern = "((?<=(,\\s))|(?<=(,))|(?<=([A-Z]{2}\\s)))\\d+"),
-         death_date = str_extract(death, pattern = "((?<=(,\\s))|(?<=(,))|(?<=([A-Z]{2}\\s)))\\d+")) |>
-  select(birth, birth_date, death, death_date)
